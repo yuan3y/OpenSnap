9da36dff376f639b534ee1d41634f58f080a2168
Chin Meng Lim <C130103@e.ntu.edu.sg>
2014-09-24 11:59:02 +0800

    update files

--- handlers/journalhandler.php
+++ handlers/journalhandler.php
@@ -1,9 +1,5 @@
 <?php
-class UsersHandler {
-	//-------------------------------------------------------------------------------------------------------------------
-	//get journal  base on journal id  - for user edit , maybe need 2 condition?
-
-	
+class JournalsHandler {
 	function get($entry_id) {//get full product details when productid or barcode is present
 		$sql = "SELECT `entry_id`,`user_id`,`product_id`,`timestamp`,`image`,`rating_ease`,`rating_safety`,`rating_reseal`,`rating_overall`,`comment`FROM `journal` WHERE `entry_id`='$entry_id'";
 		if ($result = mysqli_query($GLOBALS['con'], $sql)) {
@@ -14,45 +10,6 @@ class UsersHandler {
 			echo _response(array("error"=>mysqli_error($GLOBALS['con'])),404);
 		}
 	}
-
-	//-------------------------------------------------------------------------------------------------------------------
-	//get journal  base on user id   - own journal feed
-
-	function get($user_id) {//get full product details when productid or barcode is present
-		$sql = "SELECT `entry_id`,`user_id`,`product_id`,`timestamp`,`image`,`rating_ease`,`rating_safety`,`rating_reseal`,`rating_overall`,`comment`FROM `journal` WHERE `entry_id`='$user_id'";
-		if ($result = mysqli_query($GLOBALS['con'], $sql)) {
-			echo _response(mysqli_fetch_all($result,MYSQLI_ASSOC)[0]);
-			mysqli_free_result($result);
-		}
-		else {
-			echo _response(array("error"=>mysqli_error($GLOBALS['con'])),404);
-		}
-	}
-
-	//-------------------------------------------------------------------------------------------------------------------
-	//get journal  base on product id  - broswe feed or company such enquires
-
-		function get($product_id) {//get full product details when productid or barcode is present
-		$sql = "SELECT `entry_id`,`user_id`,`product_id`,`timestamp`,`image`,`rating_ease`,`rating_safety`,`rating_reseal`,`rating_overall`,`comment`FROM `journal` WHERE `entry_id`='$product_id'";
-		if ($result = mysqli_query($GLOBALS['con'], $sql)) {
-			echo _response(mysqli_fetch_all($result,MYSQLI_ASSOC)[0]);
-			mysqli_free_result($result);
-		}
-		else {
-			echo _response(array("error"=>mysqli_error($GLOBALS['con'])),404);
-		}
-	}
-
-	//-------------------------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
 	function post() { // 10 colums , get user inputs and insert new product to system
 		$entry_id 		= sanitize($_POST["entry_id"]); // journal entries auto generate, need this?
 		$user_id 		= sanitize($_POST["user_id "]);
--- index.php
+++ index.php
@@ -24,11 +24,15 @@ Toro::serve(array(
     "/" => "HelloHandler",
     "/test/" => "TestHandler",
     "/users/" => "UsersHandler",
-    "/users/:number" => "UsersHandler",
+    "/users/:number" => "UserHandler",
 	
 	
-	//CM ADD LINES FOR checklogin
-	 "/checklogin/" => "LoginHandler"
+	//CM ADD LINES FOR additional handlers
+	 "/checklogin/" => "LoginHandler",
+	 "/products/" => "ProductsHandler",
+	 "/products/:number" => "ProductsHandler",
+	 "/journals/" => "JournalsHandler",
+	 "/journal/:number" => "JournalsHandler"
 ));
 
 ?>
