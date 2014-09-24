927c58e7951b96c4d418349c372b4210b657c528
Chin Meng Lim <C130103@e.ntu.edu.sg>
2014-09-24 17:33:41 +0800

    update entries

--- handlers/entrieshandler.php
+++ handlers/entrieshandler.php
@@ -24,11 +24,18 @@ class EntriesHandler {
 
 
 	function post() { // 10 colums , get user inputs and insert new product to system
-		$entry_id 		= sanitize($_POST["entry_id"]); // journal entries auto generate, need this?
+		//$entry_id 		= sanitize($_POST["entry_id"]); // journal entries auto generate, need this?
 		$user_id 		= sanitize($_POST["user_id "]);
 		$product_id		= sanitize($_POST["product_id"]);
-		$timestamp 		= sanitize($_POST["timestamp "]);
-		$image 			= sanitize($_POST["image "]);
+		//$timestamp 		= sanitize($_POST["timestamp "]);
+		//$image 			= sanitize($_POST["image "]);
+		
+			//correct image implemenation?
+		$image =$_FILES["file"];
+		//echo $image['name'];
+
+
+
 		$rating_ease 	= sanitize($_POST["rating_ease"]);
 		$rating_safety 	= sanitize($_POST["rating_safety"]);
 		$rating_reseal 	= sanitize($_POST["rating_reseal"]);
@@ -40,15 +47,18 @@ class EntriesHandler {
 		
 		
 		$sql = "INSERT INTO `php54`.`journal` (`entry_id`,`user_id`,`product_id`,`timestamp`,`image`,`rating_ease`,`rating_safety`,`rating_reseal`,`rating_overall`,`comment`) VALUES (NULL, '$user_id', '$product_id', '$timestamp', '$image', '$rating_ease', '$rating_safety ', '$rating_reseal', '$rating_overall','$comment');";
-		if ($result = mysqli_query($GLOBALS['con'], $sql)) {
-			$sql = "SELECT `entry_id`,`user_id`,`product_id`,`timestamp`,`image`,`rating_ease`,`rating_safety`,`rating_reseal`,`rating_overall`,`comment`FROM `journal` WHERE `entry_id`='$entry_id'";
-			if ($result = mysqli_query($GLOBALS['con'], $sql)) {
-				echo _response(mysqli_fetch_all($result,MYSQLI_ASSOC)[0],201);
+		if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
+			$resultArray = mysqli_fetch_all($result,MYSQLI_ASSOC);
+			if (empty($resultArray)){
+				echo _response(array("error"=>"login does not match , error"),404);
+				}
+			else{
+				echo _response($resultArray[0],200);
 				mysqli_free_result($result);
 			}
 		}
-		else {
-			echo _response(array("error"=>mysqli_error($GLOBALS['con'])),422);
+		else{ //SQL (grammar) has error
+			echo _response(array("error"=>mysqli_error($GLOBALS['con'])),500);
 		}
 	}
 	
