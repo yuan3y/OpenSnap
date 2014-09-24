328a4e34cc3d02f7a2da8d192098e859b2ce23da
Chin Meng Lim <C130103@e.ntu.edu.sg>
2014-09-24 16:51:37 +0800

    journal entries handler update

--- handlers/entrieshandler.php
+++ handlers/entrieshandler.php
@@ -1,15 +1,28 @@
 <?php
 class EntriesHandler {
-	function get($entry_id) {//get full product details when productid or barcode is present
-		$sql = "SELECT `entry_id`,`user_id`,`product_id`,`timestamp`,`image`,`rating_ease`,`rating_safety`,`rating_reseal`,`rating_overall`,`comment`FROM `journal` WHERE `entry_id`='$entry_id'";
-		if ($result = mysqli_query($GLOBALS['con'], $sql)) {
-			echo _response(mysqli_fetch_all($result,MYSQLI_ASSOC)[0]);
-			mysqli_free_result($result);
+
+
+//SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `user_id`='' AND `product_id`=''
+
+
+	function get($user_id ,$product_id) {//get full product details when productid or barcode is present
+		$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `user_id`='$user_id' AND `product_id`='$product_id'";
+		if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
+			$resultArray = mysqli_fetch_all($result,MYSQLI_ASSOC);
+			if (empty($resultArray)){
+				echo _response(array("error"=>"entries does not exists , error"),404);
+				}
+			else{
+				echo _response($resultArray[0],200);
+				mysqli_free_result($result);
+			}
 		}
-		else {
-			echo _response(array("error"=>mysqli_error($GLOBALS['con'])),404);
+		else{ //SQL (grammar) has error
+			echo _response(array("error"=>mysqli_error($GLOBALS['con'])),500);
 		}
 	}
+
+	
 	function post() { // 10 colums , get user inputs and insert new product to system
 		$entry_id 		= sanitize($_POST["entry_id"]); // journal entries auto generate, need this?
 		$user_id 		= sanitize($_POST["user_id "]);
