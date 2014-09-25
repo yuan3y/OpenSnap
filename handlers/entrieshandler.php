5f18cd2ff15a258a51dba616202e66b4c340cc85
Chin Meng Lim <C130103@e.ntu.edu.sg>
2014-09-25 20:45:31 +0800

    try to connect entries ahndlers

--- handlers/entrieshandler.php
+++ handlers/entrieshandler.php
@@ -2,20 +2,14 @@
 class EntriesHandler{
 
 
-	function post() {
-		$entry_id 		= sanitize($_POST["entry_id"]); 
-		$user_id 		= sanitize($_POST["user_id"]);
-		$product_id		= sanitize($_POST["product_id"]);
-		//$timestamp 		= sanitize($_POST["timestamp "]);
-		//$image 			= sanitize($_POST["image "]);
-		$rating_ease 	= sanitize($_POST["rating_ease"]);
-		$rating_safety 	= sanitize($_POST["rating_safety"]);
-		$rating_reseal 	= sanitize($_POST["rating_reseal"]);
-		$rating_overall = sanitize($_POST["rating_overall"]);//need to get?
-		$comment 		= sanitize($_POST["comment"]); 
+	function post($user_id) { // for user journal feed
+		//$user_id 		= sanitize($_POST["user_id"]);
+		var_dump($userid);
 
 
 		$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `user_id`='$user_id'";
+		
+		var_dump($sql);
 
 		if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
 			$resultArray = mysqli_fetch_all($result,MYSQLI_ASSOC);
