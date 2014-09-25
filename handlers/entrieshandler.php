adafe1c474c1a768dbca0994507bff18be261f73
Chin Meng Lim <C130103@e.ntu.edu.sg>
2014-09-25 13:48:44 +0800

    update entired handler

--- handlers/entrieshandler.php
+++ handlers/entrieshandler.php
@@ -27,8 +27,8 @@ class EntriesHandler{
 
 	function post() { 
 		//echo"ENTERING ENTRIES HANDLER post";
-		//$entry_id 		= sanitize($_POST["entry_id"]); 
-		$user_id 		= sanitize($_POST["user_id "]);
+		$entry_id 		= sanitize($_POST["entry_id"]); 
+		$user_id 		= sanitize($_POST["user_id"]);
 		$product_id		= sanitize($_POST["product_id"]);
 		//$timestamp 		= sanitize($_POST["timestamp "]);
 		//$image 			= sanitize($_POST["image "]);
@@ -38,7 +38,7 @@ class EntriesHandler{
 		$rating_overall = sanitize($_POST["rating_overall"]);//need to get?
 		$comment 		= sanitize($_POST["comment"]);
 	
-		if (empty($_POST["entry_id"])) {
+		if (empty($_POST["entry_id"])) { // handles new entries
 			$sql = "SELECT COUNT(*) AS cnt FROM `entry` WHERE `user_id`='$user_id' AND `product_id`='$product_id'";
 			if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
 				$resultArray = mysqli_fetch_all($result,MYSQLI_ASSOC);
@@ -76,15 +76,45 @@ class EntriesHandler{
 				echo _response(array("error"=>mysqli_error($GLOBALS['con'])),500);
 			}
 		}
-		else {
-
-
-
-
-
-
-
-			
+		else //Handles update entries
+		{
+			$sql = "SELECT COUNT(*) AS cnt FROM `entry` WHERE `entry_id`='$entry_id'";
+			if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
+				$resultArray = mysqli_fetch_all($result,MYSQLI_ASSOC);
+				if ($resultArray[0]['cnt']==0){
+					//
+					$sql = "UPDATE `entry` SET `rating_ease`='$rating_ease',`rating_safety`='$rating_safety',`rating_reseal`='$rating_reseal',`rating_overall`='$rating_overall',`comment`='$comment' WHERE `entry_id`='$enrty_id'";
+					if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
+						if ($result){
+							$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `user_id`='$user_id' AND `product_id`='$product_id'";
+							if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
+								$resultArray = mysqli_fetch_all($result,MYSQLI_ASSOC);
+								if (empty($resultArray)){ 
+									echo _response(array("error"=>"unable to insert the user, error"),406);
+								}
+								else{
+									echo _response($resultArray[0],201);
+									mysqli_free_result($result);
+								}
+							}
+							else{ //SQL (grammar) has error
+								echo _response(array("error"=>mysqli_error($GLOBALS['con'])),500);
+							}
+						}
+					}
+					else{ //SQL (grammar) has error
+						echo _response(array("error"=>mysqli_error($GLOBALS['con'])),500);
+					}
+				}
+				else {
+					var_dump($resultArray);
+					echo "test";
+					echo _response(array("error"=>"user duplicated, error"),404);
+				}
+			}
+			else{ //SQL (grammar) has error
+				echo _response(array("error"=>mysqli_error($GLOBALS['con'])),500);
+			}
 		}
 
 	}
