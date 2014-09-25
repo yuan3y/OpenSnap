9f90e73d2a62e26ed06adf5212164024f168c4d9
Yuan Yiyang <yuan3y@gmail.com>
2014-09-26 01:01:09 +0800

    try openshift env

--- base.php
+++ base.php
@@ -18,8 +18,7 @@ function sanitize($data)
 }
 
 //$GLOBALS['con']=mysqli_connect("localhost","root","","php54");
-$addr="$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT";
-$con=mysqli_connect($addr,"adminstzRqnc","uAs_UVmwpm7p","php54");
+$con=mysqli_connect("$OPENSHIFT_MYSQL_DB_HOST","$OPENSHIFT_MYSQL_DB_USERNAME","$OPENSHIFT_MYSQL_DB_PASSWORD","$OPENSHIFT_APP_NAME");
 //$con=mysqli_connect("127.6.113.130:3306","adminstzRqnc","uAs_UVmwpm7p","php54");
 
 if (mysqli_connect_errno()) {
