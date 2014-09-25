1681ee62e91a185f3ea4768bf55555deb3553023
Chin Meng Lim <C130103@e.ntu.edu.sg>
2014-09-25 23:55:45 +0800

    updating producthandler

--- handlers/producthandler.php
+++ handlers/producthandler.php
@@ -22,10 +22,10 @@ class ProductsHandler {
 		//avg_rating need to be calculated here? or system output
 		//$avg_rating   		= $sum_of_rating / $no_of_raters;
 
-		$sql = "SELECT COUNT(*) AS cnt FROM `product` WHERE `product_id`='$product_id'";
+		$sql = "SELECT COUNT(*) AS cnt FROM `product` WHERE `product_id`='$product_id'";//>>>check whether existing product exist<<<
 				if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
 					$resultArray = mysqli_fetch_all($result,MYSQLI_ASSOC);
-					if ($resultArray[0]['cnt']==0){//insert product
+					if ($resultArray[0]['cnt']==0){//>>>insert product<<<
 						$sql = "INSERT IGNORE INTO `php54`.`product` (`product_id`,`name`,`manufacturer`,`packaging_type`,`sum_of_rating`,`no_of_raters`) VALUES ('$product_id', '$name', '$manufacturer', '$packaging_type', '$sum_of_rating','$no_of_raters')";
 						if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
 							if ($result){
@@ -49,7 +49,7 @@ class ProductsHandler {
 							echo _response(array("error"=>mysqli_error($GLOBALS['con'])),500);
 						}
 					}
-					else {//Handles update product
+					else {//>>>Handles update product<<<
 						$sql = "UPDATE `product` SET `name`='$name',`manufacturer`='$manufacturer',`packaging_type`='$packaging_type',`sum_of_rating`='$sum_of_rating',`no_of_raters`='$no_of_raters' WHERE `product_id`='$product_id'";
 						if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
 							if ($result){
@@ -76,33 +76,6 @@ class ProductsHandler {
 				}
 			}
 		}
-
-		//`avg_rating` not added in the follwoing SQL
-		/*
-		$sql = "INSERT IGNORE INTO `php54`.`product` (`product_id`,`name`,`manufacturer`,`packaging_type`,`sum_of_rating`,`no_of_raters`) VALUES ('$product_id', '$name', '$manufacturer', '$packaging_type', '$sum_of_rating','$no_of_raters')";
-		
-		if ($result = mysqli_query($GLOBALS['con'], $sql)) {
-			$sql = $sql = "SELECT `product_id`,`name`,`manufacturer`,`packaging_type`,`image`,`sum_of_rating`,`no_of_raters`,`avg_rating`FROM `product` WHERE `product_id`='$product_id'";
-			if ($result = mysqli_query($GLOBALS['con'], $sql)) {
-				echo _response(mysqli_fetch_all($result,MYSQLI_ASSOC)[0],201);
-				mysqli_free_result($result);
-			}
-		}
-		else {
-			echo _response(array("error"=>mysqli_error($GLOBALS['con'])),422);
-		}
-	}*/
-	/*
-	function delete() {
-		$user_id 	= sanitize($_REQUEST["user_id"]);
-		$sql = "DELETE FROM `php54`.`user` WHERE `user`.`user_id` = $user_id;";
-		if ($result = mysqli_query($GLOBALS['con'], $sql)) {
-			echo _response("",204);
-			mysqli_free_result($result);
-		}
-	}
-	*/
-
 
 ?>
 
