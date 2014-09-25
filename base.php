97c558d120ba08115ac9eaed1b4a923467a884a7
Yuan Yiyang <yuan3y@gmail.com>
2014-09-26 01:27:59 +0800

    try openshift env

--- base.php
+++ base.php
@@ -19,12 +19,12 @@ function sanitize($data)
 
 var_dump($_ENV);
 //$GLOBALS['con']=mysqli_connect("localhost","root","","php54");
-$sqladdr = $_ENV['OPENSHIFT_MYSQL_DB_HOST'].'.'.$_ENV['OPENSHIFT_MYSQL_DB_PORT'];
+$sqladdr = $_ENV['OPENSHIFT_MYSQL_DB_HOST'].':'.$_ENV['OPENSHIFT_MYSQL_DB_PORT'];
 $sqluser = $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'];
 $sqlpwd = $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'];
 $sqldb = $_ENV['OPENSHIFT_APP_NAME'];
 var_dump($sqladdr,$sqluser,$sqlpwd,$sqldb);
-//$con=mysqli_connect("$sqladdr","$sqluser","$sqlpwd","$sqldb");
+$con=mysqli_connect("$sqladdr","$sqluser","$sqlpwd","$sqldb");
 //$con=mysqli_connect("127.6.113.130:3306","adminstzRqnc","uAs_UVmwpm7p","php54");
 
 if (mysqli_connect_errno()) {
