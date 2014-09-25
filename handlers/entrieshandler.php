21d4877f899c5e52a70fff661c97151566167015
Chin Meng Lim <C130103@e.ntu.edu.sg>
2014-09-25 13:34:25 +0800

    update entrieshandler

--- handlers/entrieshandler.php
+++ handlers/entrieshandler.php
@@ -4,7 +4,7 @@ class EntriesHandler{
 
 	//SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `user_id`='' AND `product_id`=''
 	function get() {
-		$entry_id 		= sanitize($_GET["entry_id"]);
+		$product_id 		= sanitize($_GET["product_id"]);
 		$user_id 		= sanitize($_GET["user_id"]);
 
 
@@ -38,49 +38,55 @@ class EntriesHandler{
 		$rating_overall = sanitize($_POST["rating_overall"]);//need to get?
 		$comment 		= sanitize($_POST["comment"]);
 	
-		
-		
-		//$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `user_id`='$user_id' AND `product_id`='$product_id'";
-		//INSERT INTO `entry`(`entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9],[value-10])
-		//$sql = "INSERT INTO `entry`( `user_id`, `product_id`,  `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment`) VALUES ('$user_id','$product_id','$rating_ease','$rating_safety','$rating_reseal','$rating_overall','$comment')";
-		
-				//$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `user_id`='$user_id' AND `product_id`='$product_id'";
-		$sql = "SELECT COUNT(*) AS cnt FROM `entry` WHERE `user_id`='$user_id' AND `product_id`='$product_id'";
-		if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
-			$resultArray = mysqli_fetch_all($result,MYSQLI_ASSOC);
-			if ($resultArray[0]['cnt']==0){
-				$sql = "INSERT IGNORE INTO `php54`.`entry` ( `user_id`, `product_id`,  `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment`) VALUES ('$user_id','$product_id','$rating_ease','$rating_safety','$rating_reseal','$rating_overall','$comment')";
-				if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
-					if ($result){
-						$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `user_id`='$user_id' AND `product_id`='$product_id'";
-						if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
-							$resultArray = mysqli_fetch_all($result,MYSQLI_ASSOC);
-							if (empty($resultArray)){ 
-								echo _response(array("error"=>"unable to insert the user, error"),406);
+		if (empty($_POST["entry_id"])) {
+			$sql = "SELECT COUNT(*) AS cnt FROM `entry` WHERE `user_id`='$user_id' AND `product_id`='$product_id'";
+			if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
+				$resultArray = mysqli_fetch_all($result,MYSQLI_ASSOC);
+				if ($resultArray[0]['cnt']==0){
+					$sql = "INSERT IGNORE INTO `php54`.`entry` ( `user_id`, `product_id`,  `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment`) VALUES ('$user_id','$product_id','$rating_ease','$rating_safety','$rating_reseal','$rating_overall','$comment')";
+					if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
+						if ($result){
+							$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `user_id`='$user_id' AND `product_id`='$product_id'";
+							if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
+								$resultArray = mysqli_fetch_all($result,MYSQLI_ASSOC);
+								if (empty($resultArray)){ 
+									echo _response(array("error"=>"unable to insert the user, error"),406);
+								}
+								else{
+									echo _response($resultArray[0],201);
+									mysqli_free_result($result);
+								}
 							}
-							else{
-								echo _response($resultArray[0],201);
-								mysqli_free_result($result);
+							else{ //SQL (grammar) has error
+								echo _response(array("error"=>mysqli_error($GLOBALS['con'])),500);
 							}
-						}
-						else{ //SQL (grammar) has error
-							echo _response(array("error"=>mysqli_error($GLOBALS['con'])),500);
 						}
 					}
+					else{ //SQL (grammar) has error
+						echo _response(array("error"=>mysqli_error($GLOBALS['con'])),500);
+					}
 				}
-				else{ //SQL (grammar) has error
-					echo _response(array("error"=>mysqli_error($GLOBALS['con'])),500);
+				else {
+					var_dump($resultArray);
+					echo "test";
+					echo _response(array("error"=>"user duplicated, error"),404);
 				}
 			}
-			else {
-				var_dump($resultArray);
-				echo "test";
-				echo _response(array("error"=>"user duplicated, error"),404);
+			else{ //SQL (grammar) has error
+				echo _response(array("error"=>mysqli_error($GLOBALS['con'])),500);
 			}
 		}
-		else{ //SQL (grammar) has error
-			echo _response(array("error"=>mysqli_error($GLOBALS['con'])),500);
+		else {
+
+
+
+
+
+
+
+			
 		}
+
 	}
 
 
