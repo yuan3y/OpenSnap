2eef1d071c8f75419c6c87834a5df8993645c4b5
Chin Meng Lim <C130103@e.ntu.edu.sg>
2014-09-25 16:25:09 +0800

    remove connection for entreshandler

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require 'base.php';
 require("toro.php");
 require("handlers/imagehandler.php");
 require("handlers/entrieshandler.php");
+require("handlers/entryhandler.php");
 require("handlers/loginhandler.php");
 require("handlers/producthandler.php");
 require("handlers/userhandler.php");
