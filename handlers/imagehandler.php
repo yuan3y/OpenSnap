7dcb7c730d1b1edea5695ce7e4c223ad64b16287
Chin Meng Lim <C130103@e.ntu.edu.sg>
2014-09-26 13:07:43 +0800

    update image hanlder

--- handlers/imagehandler.php
+++ handlers/imagehandler.php
@@ -122,7 +122,8 @@ class ImageHandler {
 					//echo $_FILES["image_file"]["name"] . " already exists. ";
 					echo _response(array("error"=>$image_path . " already exists. "),409);
 				}else {
-					move_uploaded_file($_FILES["image_file"]["tmp_name"],$image_path);
+					move_uploaded_file($_FILES["tmp_name"],$image_path);
+					echo "$_FILES["tmp_name"]"; var_dump($_FILES["tmp_name"]);
 						//"upload/" . $_FILES["image_file"]["name"]);
 					//echo "Stored in: " . "upload/" . $_FILES["image_file"]["name"];
 					//echo "stored in : " . $image_path;
