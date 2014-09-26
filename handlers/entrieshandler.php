5260e97ffdd9c9d46525ce80b5a5b9e6fb154b3d
Chin Meng Lim <C130103@e.ntu.edu.sg>
2014-09-26 16:58:08 +0800

    update entries handler

--- handlers/entrieshandler.php
+++ handlers/entrieshandler.php
@@ -11,7 +11,10 @@ class EntriesHandler{
 
 		//$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `user_id`='$user_id'";
 		//$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `entry_name`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `user_id`='$user_id'";
-		$sql ="SELECT * FROM `entry` WHERE `user_id`=`$user_id`";
+		//$sql ="SELECT * FROM `entry` WHERE `user_id`=`$user_id`";
+		$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `entry_name`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `user_id`='$user_id' AND `product_id`='$product_id'";
+
+		
 		var_dump($sql);
 
 		if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
