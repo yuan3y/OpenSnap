f1f22e1334283fd26c05f3dd316dcd85f8b0f9c0
Yuan Yiyang <yuan3y@gmail.com>
2014-09-27 08:41:59 +0800

    fix imagehandler, now in permanent storage

--- handlers/imagehandler.php
+++ handlers/imagehandler.php
@@ -37,9 +37,7 @@ class ImageHandler {
 	}
 
 	function post($entry_id) {
-		//$fix_path = $_ENV["OPENSHIFT_DATA_DIR"]."upload/";
 		$fix_path = "upload/";
-		var_dump($fix_path);
 		//only process under the condition that $entry_id exists in the entry table.
 		$sql =  "SELECT `image` FROM `entry` WHERE `entry_id` = '$entry_id'";
 		if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
