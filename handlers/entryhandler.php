e295d91c6fc55b8569d1c489f6a7b6fc0a22296e
Chin Meng Lim <C130103@e.ntu.edu.sg>
2014-09-29 19:51:47 +0800

    update entry handler

--- handlers/entryhandler.php
+++ handlers/entryhandler.php
@@ -1,17 +1,17 @@
 <?php
 class EntryHandler{
 function get() {
-		//$product_id 		= sanitize($_GET["product_id"]);
-		//$user_id 		= sanitize($_GET["user_id"]);
-		$entry_id 	= sanitize($_GET["entry_id"]); 
+		$product_id 		= sanitize($_GET["product_id"]);
+		$user_id 		= sanitize($_GET["user_id"]);
+		//$entry_id 	= sanitize($_GET["entry_id"]); 
 
-		//$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `entry_name`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `user_id`='$user_id' AND `product_id`='$product_id'";
-		$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `entry_name`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `entry_id`='$entry_id'";
-		//var_dump($sql);
+		$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `entry_name`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `user_id`='$user_id' AND `product_id`='$product_id'";
+		//$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `entry_name`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `entry_id`='$entry_id'";
+		var_dump($sql);
 		if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
 			$resultArray = mysqli_fetch_all($result,MYSQLI_ASSOC);
 			if (empty($resultArray)){
-				echo _response(array("error"=>"Entries Not Found , error"),404);
+				echo _response(array("error"=>"Entries Not Found"),404);
 				}
 			else{
 				echo _response($resultArray[0],200);
