5a0399b20eedff3c56b42a7912bb567c618aab12
Chin Meng Lim <C130103@e.ntu.edu.sg>
2014-09-29 19:04:03 +0800

    update entry handler

--- handlers/entryhandler.php
+++ handlers/entryhandler.php
@@ -132,7 +132,7 @@ function get() {
 		//$sql="UPDATE `product` SET `sum_of_rating`='',`no_of_raters`='' WHERE `product_id`='$product_id'";
 
 	}*/
-
+/*
 		function Update_Product_Ratings(){ // will do the update of overall rating and counter for product
 		//will need to ->>sum_of_rating +=(current)user rating - (prevoious)user_rating, 
 		$New_sum_of_rating =0;
@@ -166,7 +166,7 @@ function get() {
 
 
 	}
+*/
 
-}
 
 ?>
