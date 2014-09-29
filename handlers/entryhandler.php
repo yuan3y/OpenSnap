0fd04ca49c2de3d9f051965eacb9c17e524fe914
Chin Meng Lim <C130103@e.ntu.edu.sg>
2014-09-29 18:59:45 +0800

    update entry handler

--- handlers/entryhandler.php
+++ handlers/entryhandler.php
@@ -1,10 +1,12 @@
 <?php
 class EntryHandler{
 function get() {
-		$product_id 		= sanitize($_GET["product_id"]);
-		$user_id 		= sanitize($_GET["user_id"]);
+		//$product_id 		= sanitize($_GET["product_id"]);
+		//$user_id 		= sanitize($_GET["user_id"]);
+		$entry_id 		= sanitize($_POST["entry_id"]); 
 
-		$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `entry_name`,, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `user_id`='$user_id' AND `product_id`='$product_id'";
+		//$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `entry_name`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `user_id`='$user_id' AND `product_id`='$product_id'";
+		$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `entry_name`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `entry_id`='$entry_id'";
 		if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
 			$resultArray = mysqli_fetch_all($result,MYSQLI_ASSOC);
 			if (empty($resultArray)){
@@ -117,5 +119,54 @@ function get() {
 		}
 	}
 }
+
+
+
+	function New_Product_Rating_Changer(){ // will do the update of overall rating and counter for product
+		//will need to ->>sum_of_rating +=user rating, no_of_raters++;
+
+		//1. need to get the current vaules and store them
+		$sql = "SELECT`sum_of_rating`, `no_of_raters` FROM `product` WHERE `product_id`=`$product_id`";
+		//store the data into a variable
+		//2. update the current data by adding the current entry overall rating in and increase the counter by 1 and store it back
+		//$sql="UPDATE `product` SET `sum_of_rating`='',`no_of_raters`='' WHERE `product_id`='$product_id'";
+
+	}
+
+		function Update_Product_Ratings(){ // will do the update of overall rating and counter for product
+		//will need to ->>sum_of_rating +=(current)user rating - (prevoious)user_rating, 
+		$New_sum_of_rating =0;
+		$New_no_of_raters=0;
+
+		$sql_selectAll = "SELECT `product_id`,`name`,`manufacturer`,`packaging_type`,`image`,`sum_of_rating`,`no_of_raters`,`avg_rating`FROM `product` WHERE `product_id`='$product_id'";
+		//1. need to get the current vaules and store them
+		$sql_select = "SELECT`sum_of_rating`, `no_of_raters` FROM `product` WHERE `product_id`=`$product_id`"
+		//store the data into a variable
+		//2. update the current data by adding the current entry overall rating in and increase the counter by 1 and store it back
+		$sql_update="UPDATE `product` SET `sum_of_rating`='',`no_of_raters`='' WHERE `product_id`='$product_id'";
+		//(if result is null);store '0'it to a set of variable, else store the result into the same set of variable for later use
+
+
+
+		if ($result = mysqli_query($GLOBALS['con'], $sql_select)) { //SQL (grammar) is correctly executed
+				$New_sum_of_rating = $result[0] +$sum_of_rating; //current sum_of_rating + new entry rating
+				$New_no_of_raters= $result[1]+1;				//current no_of_rater ++;
+				if ($result = mysqli_query($GLOBALS['con'], $sql_update)) {
+					$result = mysqli_query($GLOBALS['con'], $sql_selectAll);
+					echo _response($resultArray[0],201);
+					mysqli_free_result($result);
+				}
+				else{
+					echo _response(array("error"=>mysqli_error($GLOBALS['con'])),500);
+				}
+		}
+		else{
+			echo _response(array("error"=>mysqli_error($GLOBALS['con'])),500);
+		}
+
+
+	}
+
+
 
 ?>
