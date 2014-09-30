c32c1fc2e0d4f7609e3d1ba522c7998dcaa69a33
Chin Meng Lim <C130103@e.ntu.edu.sg>
2014-09-30 11:32:23 +0800

    adding 2 new col into entry db

--- handlers/entryhandler.php
+++ handlers/entryhandler.php
@@ -42,7 +42,7 @@ function get() {
 			if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
 				$resultArray = mysqli_fetch_all($result,MYSQLI_ASSOC);
 				if ($resultArray[0]['cnt']==0){
-					$sql = "INSERT IGNORE INTO `php54`.`entry` ( `user_id`, `product_id`, `entry_name`,`manufacturer`,`packaging_type`,`rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment`) VALUES ('$user_id','$product_id','$entry_name','$manufacturer','$packaging_type',$rating_ease','$rating_safety','$rating_reseal','$rating_overall','$comment')";
+					$sql = "INSERT IGNORE INTO `php54`.`entry` ( `user_id`, `product_id`, `entry_name`, `manufacturer`, `packaging_type`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment`) VALUES ('$user_id','$product_id','$entry_name','$manufacturer','$packaging_type',$rating_ease','$rating_safety','$rating_reseal','$rating_overall','$comment')";
 					var_dump($result);
 					if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
 						if ($result){
