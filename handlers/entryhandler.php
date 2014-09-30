b96f088f1d9b32acdf2f2fbfc6e0005fe1c84de9
Chin Meng Lim <C130103@e.ntu.edu.sg>
2014-09-30 12:13:26 +0800

    adding 2 new col into entry db

--- handlers/entryhandler.php
+++ handlers/entryhandler.php
@@ -104,7 +104,7 @@ function get() {
 			$sql = "UPDATE `entry` SET `entry_name`='$entry_name', `manufacturer`='$manufacturer', `packaging_type`='$packaging_type',`rating_ease`='$rating_ease',`rating_safety`='$rating_safety',`rating_reseal`='$rating_reseal',`rating_overall`='$rating_overall',`comment`='$comment' WHERE `entry_id`='$entry_id'";
 			if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
 				if ($result){
-					$sql = "SELECT`entry_id`, `user_id`, `product_id`, `timestamp`, `image`,`entry_name`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment`  FROM `entry` WHERE `entry_id`='$entry_id'";
+					$sql = "SELECT`entry_id`, `user_id`, `product_id`, `timestamp`, `image`,`entry_name`,manufacturer`,`packaging_type`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment`  FROM `entry` WHERE `entry_id`='$entry_id'";
 					if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
 						$resultArray = mysqli_fetch_all($result,MYSQLI_ASSOC);
 						if (empty($resultArray)){ 
