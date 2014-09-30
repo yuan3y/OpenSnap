d190c66f7d2063b293509e6de68f4ca622e6f51e
Chin Meng Lim <C130103@e.ntu.edu.sg>
2014-09-30 11:51:48 +0800

    adding 2 new col into entry db

--- handlers/entryhandler.php
+++ handlers/entryhandler.php
@@ -73,7 +73,7 @@ function get() {
 						if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
 							if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
 								if ($result){
-									$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`,`entry_name`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `user_id`='$user_id' AND `product_id`='$product_id'";
+									$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`,`entry_name`,`manufacturer`,`packaging_type`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `user_id`='$user_id' AND `product_id`='$product_id'";
 									if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
 										$resultArray = mysqli_fetch_all($result,MYSQLI_ASSOC);
 										if (empty($resultArray)){ 
