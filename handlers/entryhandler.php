d356799da6e938813252b77f91eb9629d3179c9d
Chin Meng Lim <C130103@e.ntu.edu.sg>
2014-10-02 15:23:41 +0800

    fix name

--- handlers/entryhandler.php
+++ handlers/entryhandler.php
@@ -5,7 +5,7 @@ function get() {
 		$user_id 		= sanitize($_GET["user_id"]);
 		//$entry_id 	= sanitize($_GET["entry_id"]); 
 
-		$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `user_id`='$user_id' AND `product_id`='$product_id'";
+		$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `name` AS `entry_name`, `image`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `user_id`='$user_id' AND `product_id`='$product_id'";
 		//$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `entry_name`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `entry_id`='$entry_id'";
 		//var_dump($sql);
 		if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
