089b2bea80229aac15631880738eab763ba50711
Chin Meng Lim <C130103@e.ntu.edu.sg>
2014-10-02 09:59:58 +0800

    update entreies handler

--- index.php
+++ index.php
@@ -40,10 +40,8 @@ Toro::serve(array(
 	"/products/" => "ProductsHandler",
 	"/products/:number" => "ProductsHandler",
 	"/entries/" => "EntryHandler",
-	"/entries/:number" => "EntriesHandler", //<--CM, pls remove this line --YY>
-	"/users/:number/entries/" => "EntriesHandler", //<-- this is what u meant to have get($user_id) in EntriesHandler class
+	"/users/:number/entries/" => "EntriesHandler",
 	"/entries/:number/image/" => "ImageHandler",
-	"/journals/" => "EntriesHandler", //<-- test handler. same as entries handler BOTH STILL cause the index to go error
 	"/entries/:number/delete/" => "DeleteHandler"
 
 
