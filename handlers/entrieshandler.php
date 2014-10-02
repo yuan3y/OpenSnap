35852b4909c17822fcdef3629998ba94e8af5593
Chin Meng Lim <C130103@e.ntu.edu.sg>
2014-10-02 16:35:50 +0800

    fix entries handler change sql statement

--- handlers/entrieshandler.php
+++ handlers/entrieshandler.php
@@ -6,7 +6,7 @@ class EntriesHandler{
 
 	function post($user_id) { // for user journal feed
 		//$user_id 		= sanitize($_POST["user_id"]);
-		var_dump($user_id);
+		if (isset($GLOBALS['DEBUG']) && $GLOBALS['DEBUG']) var_dump($user_id);
 
 
 		//$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `user_id`='$user_id'";
@@ -15,7 +15,7 @@ class EntriesHandler{
 		//$sql = "SELECT `entry_id`, `user_id`, `product_id`, `timestamp`, `image`, `entry_name`, `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` WHERE `user_id`='$user_id'ORDER BY `entry_id` DESC";
 		//$sql="SELECT `entry_id` ,  `user_id` ,  `product_id` ,  `timestamp` ,  `image` ,  `name` ,  `rating_ease` ,  `rating_safety` ,  `rating_reseal` , `rating_overall` ,  `comment` FROM  `entry` WHERE  `user_id` = '2'ORDER BY  `timestamp` DESC ";
 		$sql = "SELECT `entry_id`, `user_id`, `entry`.`product_id`, `timestamp`, `entry`.`image`,`entry`.`name`, `manufacturer`, `packaging_type`,  `rating_ease`, `rating_safety`, `rating_reseal`, `rating_overall`, `comment` FROM `entry` INNER JOIN `product` ON `entry`.`product_id` = `product`.`product_id` WHERE `user_id`='$user_id' ";
-		var_dump($sql);
+		if (isset($GLOBALS['DEBUG']) && $GLOBALS['DEBUG']) var_dump($sql);
 
 		if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
 			$resultArray = mysqli_fetch_all($result,MYSQLI_ASSOC);
