e34973c4b60a675061a9582a039f02199d87664b
Yuan Yiyang <yuan3y@gmail.com>
2014-10-08 21:44:05 +0800

    completed both get and post method

--- webview-test/get-post.php
+++ webview-test/get-post.php
@@ -1,31 +1,27 @@
 <?php
-$method = 'POST';
-$url = 'http://localhost/users/';
-$data = http_build_query(array(
-    'email' => 'getpost3@example.com', 
-    'password' => 'getpost3', 
-    'name' => 'GetPost', 
-    'gender' => '0', 
-    'age' => '29'
-)
-);
+$method = 'GET'; //change to 'POST' for post method
+$url = 'http://localhost/browse/';
+$data = array(
+	'manufacturer' => 'kraft',
+	'packaging_type' => 'bag'
+	);
 
-$response = '';
-// Create HTTP stream context
 if ($method == 'POST'){
-$context = stream_context_create(array(
-    'http' => array(
-        'method' => "$method",
-        'header' => 'Content-Type: application/x-www-form-urlencoded',
-        'content' => $data)
-    )
-);
-$response = file_get_contents($url, false, $context);
+//Make POST request
+	$data = http_build_query($data);
+	$context = stream_context_create(array(
+		'http' => array(
+			'method' => "$method",
+			'header' => 'Content-Type: application/x-www-form-urlencoded',
+			'content' => $data)
+		)
+	);
+	$response = file_get_contents($url, false, $context);
 }
 else {
-	$queryString = http_build_query($data, '', '&');
+// Make GET request
+	$data = http_build_query($data, '', '&');
 	$response = file_get_contents($url."?".$data, false);
 }
-// Make POST request
 echo $response;
 ?>
