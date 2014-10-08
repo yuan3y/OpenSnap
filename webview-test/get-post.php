72803c4ff5ae2c0fa31eb1ec6fb9c47b0cd4892d
Yuan Yiyang <yuan3y@gmail.com>
2014-10-08 20:49:15 +0800

    post method

--- webview-test/get-post.php
+++ webview-test/get-post.php
@@ -1,18 +1,31 @@
 <?php
+$method = 'POST';
+$url = 'http://localhost/users/';
 $data = http_build_query(array(
-    'param1' => 'data1',
-    'param2' => 'data2'
-));
+    'email' => 'getpost3@example.com', 
+    'password' => 'getpost3', 
+    'name' => 'GetPost', 
+    'gender' => '0', 
+    'age' => '29'
+)
+);
 
+$response = '';
 // Create HTTP stream context
+if ($method == 'POST'){
 $context = stream_context_create(array(
     'http' => array(
-        'method' => 'POST',
+        'method' => "$method",
         'header' => 'Content-Type: application/x-www-form-urlencoded',
-        'content' => $data
+        'content' => $data)
     )
-));
-
+);
+$response = file_get_contents($url, false, $context);
+}
+else {
+	$queryString = http_build_query($data, '', '&');
+	$response = file_get_contents($url."?".$data, false);
+}
 // Make POST request
-$response = file_get_contents('http://example.com', false, $context);
+echo $response;
 ?>
