9e36d5b78d5dafe2ac055efcfbf3ebb0c2ac3556
Chin Meng Lim <C130103@e.ntu.edu.sg>
2014-10-10 20:33:25 +0800

    fix delete handler on delete product when raters and avg rating reaches 0

--- handlers/rating_segment.php
+++ handlers/rating_segment.php
@@ -12,8 +12,15 @@ WHERE (
 `entry`.`product_id` =  '$product_id'
 ) ) 
 WHERE  `product_id` =  '$product_id'";
+
+
+$sql_delete = "DELETE FROM  `product` WHERE  `no_of_raters` =0 AND  `avg_rating` =0";
+
+
 		if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
 			
+			mysqli_query($GLOBALS['con'], $sql_delete);// do system clean up, delete product that have 0 raters and 0 rating.
+
 		}
 		else{ //SQL (grammar) has error
 			echo _response(array("error"=>mysqli_error($GLOBALS['con'])),500);
