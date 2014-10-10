57af434786b4b47512380815656945c1ea1ab9c5
Chin Meng Lim <C130103@e.ntu.edu.sg>
2014-10-10 18:12:42 +0800

    fix delete handler on rating issue. inculde raing segment, API changed

--- handlers/deletehandler.php
+++ handlers/deletehandler.php
@@ -7,10 +7,12 @@ class DeleteHandler{
 
 
 	function post($entry_id) {
+		$product_id		= sanitize($_POST["product_id"]);
 		$sql = "DELETE FROM `entry` WHERE `entry_id`='$entry_id'";
 		//var_dump($sql);
 		if ($result = mysqli_query($GLOBALS['con'], $sql)) { //SQL (grammar) is correctly executed
 			//$GLOBALS['DEBUG']=true;
+			include "rating_segment.php";
 			echo _response(array("success"=>"entry  $entry_id has been successfully deleleted"),202);
 			//echo _response(array("error"=>"entry  $entry_id has been successfully deleleted"),204);
 			//mysqli_free_result($result);
